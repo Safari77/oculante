be1dddacbd6d812f4c8557e165238a213b4fd476
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2025-12-04 21:04:28 +0200

    write saved images securely (tmp file+fsync+rename)

--- src/file_encoder.rs
+++ src/file_encoder.rs
@@ -10,9 +10,11 @@ use image::{DynamicImage, ImageEncoder};
 use notan::egui::Ui;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::io::BufWriter;
 use std::path::Path;
+use std::io::{BufWriter, Write};
 use strum::{Display, EnumIter};
+use tempfile::Builder;
+use anyhow::Context;
 
 #[derive(Default, Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Display, EnumIter)]
 
@@ -64,37 +66,60 @@ impl FileEncoder {
     }
 
     pub fn save(&self, image: &DynamicImage, path: &Path) -> Result<()> {
-        match self {
-            FileEncoder::Jpg { quality } => {
-                let w = File::create(path)?;
-                JpegEncoder::new_with_quality(w, *quality as u8).encode_image(image)?;
-            }
-            FileEncoder::Png { compressionlevel } => {
-                let file = File::create(path)?;
-                let writer = BufWriter::new(file);
-                let encoder = PngEncoder::new_with_quality(
-                    writer,
-                    match compressionlevel {
+        let parent_dir = path.parent().unwrap_or_else(|| Path::new("."));
+        let mut tmp_file = Builder::new()
+            .suffix(".tmp")
+            .tempfile_in(parent_dir)
+            .context("Failed to create temporary file")?;
+
+        {
+            // Set buffer to 64KB (65536 bytes) instead of the default 8KB
+            let mut writer = BufWriter::with_capacity(64 * 1024, &mut tmp_file);
+
+            match self {
+                FileEncoder::Jpg { quality } => {
+                    let rgb_image = image.to_rgb8();
+                    JpegEncoder::new_with_quality(&mut writer, *quality as u8)
+                        .write_image(
+                            rgb_image.as_raw(),
+                            rgb_image.width(),
+                            rgb_image.height(),
+                            image::ExtendedColorType::Rgb8,
+                        )?;
+                }
+                FileEncoder::Png { compressionlevel } => {
+                    let compression = match compressionlevel {
                         CompressionLevel::Best => CompressionType::Best,
                         CompressionLevel::Default => CompressionType::Default,
                         CompressionLevel::Fast => CompressionType::Fast,
-                    },
-                    image::codecs::png::FilterType::default(),
-                );
-                encoder.write_image(
-                    image.as_bytes(),
-                    image.width(),
-                    image.height(),
-                    image::ExtendedColorType::Rgba8,
-                )?;
+                    };
+
+                    PngEncoder::new_with_quality(
+                        &mut writer,
+                        compression,
+                        image::codecs::png::FilterType::default(),
+                    )
+                    .write_image(
+                        image.as_bytes(),
+                        image.width(),
+                        image.height(),
+                        image.color().into(),
+                    )?;
+                }
+                FileEncoder::Bmp => {
+                    image.write_to(&mut writer, image::ImageFormat::Bmp)?;
+                }
+                FileEncoder::WebP => {
+                    image.write_to(&mut writer, image::ImageFormat::WebP)?;
+                }
             }
-            FileEncoder::Bmp => {
-                image.save_with_format(path, image::ImageFormat::Bmp)?;
-            }
-            FileEncoder::WebP => {
-                image.save_with_format(path, image::ImageFormat::WebP)?;
-            }
-        }
+        } // Buffer flushes here
+
+        // Fsync to disk
+        tmp_file.as_file().sync_all().context("Failed to sync to disk")?;
+
+        // Atomic rename
+        tmp_file.persist(path).context("Failed to persist file")?;
 
         Ok(())
     }
